4b66f64249efa6a55c281bbd997ced700ea5b98f
Robert Kardos <kardos.robert.kh@gmail.com>
2024-04-22 02:07:28 +0200

    Add Yeelight library and bulb toggle function

--- dbus-service.py
+++ dbus-service.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+from yeelight import discover_bulbs, Bulb
+
 from dasbus.server.interface import dbus_interface
 from dasbus.typing import Str
+
+
+data = discover_bulbs()
+ip = data[0]["ip"]
+bulb = Bulb(ip)
+
+print("==========================")
+print("BULB found on IP: ", ip)
+print("==========================")
+
 
 @dbus_interface("org.example.HelloWorld")
 class HelloWorld(object):
 
     def Hello(self, name: Str) -> Str:
+        bulb.toggle()
         return "Hello {}!".format(name)
 
 print(HelloWorld.__dbus_xml__)
